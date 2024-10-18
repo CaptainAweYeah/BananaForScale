2ce9af55a1dedca0f095515ead33424c1843b905
CaptainAweYeah <ashmorewillard@gmail.com>
2024-10-18 10:18:45 -0500

    Initial upload of banana

---
+++ banana_for_scale.py
@@ -0,0 +1,48 @@
+import os
+import bpy
+
+bl_info = {
+    "name": "Banana For Scale",
+    "author": "CaptainAweYeah",
+    "version": (1, 0),
+    "blender": (4, 00, 0),
+    "location": "View3D > Add Banana",
+    "description": "Inserts banana at 3d cursor for scale purposes",
+    "category": "3D View",
+}
+
+# banana button class
+class TEST_OT_material_init(bpy.types.Operator):
+    bl_idname = 'test.material_init'
+    bl_label = 'Banana For Scale'
+    
+    # Execute banana function
+    def execute(self, context):
+        # Find banana
+        cwd = os.getcwd()
+        bD = '/4.0/scripts/Banana For Scale/Banana.blend'
+        banana_path = cwd + bD
+
+        # Link the external Banana
+        with bpy.data.libraries.load(banana_path, link=True) as (data_from, data_to):
+            # Specify the banana
+            data_to.objects = ['Banana']
+            
+        # Access the banana object
+        linked_banana = bpy.data.objects['Banana']
+        
+        # Insert banana
+        linked_banana.location = bpy.context.scene.cursor.location
+        bpy.context.collection.objects.link(linked_banana)
+        return {'FINISHED'}
+    
+# Register banana button
+bpy.utils.register_class(TEST_OT_material_init)
+
+def menu_item_draw_func(self, context):
+    self.layout.separator()
+    self.layout.operator('test.material_init', icon='MATERIAL')
+    
+bpy.types.VIEW3D_MT_object_context_menu.append(menu_item_draw_func)
+
+    
